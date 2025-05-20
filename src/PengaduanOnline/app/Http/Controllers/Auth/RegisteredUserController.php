9388f3aa03f26cb7e5f70649d383df818cb09454
Gioezzy <giovanniyuda29@gmail.com>
2025-05-20 23:17:48 +0700

    chore: add phone number and address for table users

--- src/PengaduanOnline/app/Http/Controllers/Auth/RegisteredUserController.php
+++ src/PengaduanOnline/app/Http/Controllers/Auth/RegisteredUserController.php
@@ -33,18 +33,23 @@ class RegisteredUserController extends Controller
             'name' => ['required', 'string', 'max:255'],
             'email' => ['required', 'string', 'lowercase', 'email', 'max:255', 'unique:'.User::class],
             'password' => ['required', 'confirmed', Rules\Password::defaults()],
+            'no_hp' => ['required', 'string', 'max:15'],
+            'alamat' => ['required', 'string', 'max:255'],
         ]);
 
         $user = User::create([
             'name' => $request->name,
             'email' => $request->email,
             'password' => Hash::make($request->password),
+            'role' => 'user',
+            'no_hp' => $request->no_hp,
+            'alamat' => $request->alamat,
         ]);
 
         event(new Registered($user));
 
         Auth::login($user);
 
-        return redirect(route('dashboard', absolute: false));
+        return redirect(route('index', absolute: false));
     }
 }
