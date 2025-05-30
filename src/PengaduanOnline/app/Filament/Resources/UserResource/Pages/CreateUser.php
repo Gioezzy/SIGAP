eeb492f17974bbcac9dabc892146fc7ae1f9f7af
Gioezzy <giovanniyuda29@gmail.com>
2025-05-31 00:29:14 +0700

    feat(admin): implement user creation with role selection, validation and email verification

---
+++ src/PengaduanOnline/app/Filament/Resources/UserResource/Pages/CreateUser.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Filament\Resources\UserResource\Pages;
+
+use App\Filament\Resources\UserResource;
+use App\Models\User;
+use Filament\Actions;
+use Filament\Notifications\Notification;
+use Filament\Resources\Pages\CreateRecord;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Hash;
+
+class CreateUser extends CreateRecord
+{
+    protected static string $resource = UserResource::class;
+
+    protected function handleRecordCreation(array $data): Model
+    {
+        $data['password'] = Hash::make($data['password']);
+        $user = User::create($data);
+
+        // kirim email verifikasi ke users untuk verifikasi email
+        // $user->sendEmailVerificationNotification(); belum dipakai untuk saat ini
+        return $user;
+    }
+
+    protected function getCreatedNotification(): ?Notification
+    {
+        return Notification::make()
+            ->success()
+            ->title('Pengguna Baru Dibuat')
+            ->body('Pengguna baru telah berhasil dibuat.');
+    }
+
+
+    protected function getRedirectUrl(): string
+    {
+        return UserResource::getUrl('index');
+    }
+}
