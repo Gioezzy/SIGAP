da62cdaeb90786ebf1eea2dd69fcc1c4e4050efc
Gioezzy <giovanniyuda29@gmail.com>
2025-06-01 16:37:53 +0700

    feat: Add route and controller logic to show published berita by slug

--- src/PengaduanOnline/routes/web.php
+++ src/PengaduanOnline/routes/web.php
@@ -2,24 +2,14 @@
 
 use App\Http\Controllers\users\PengaduanController;
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\users\BeritaController;
 use App\Http\Controllers\users\TanggapanPengaduanController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('index');
-})->name('index');
 
-// Route::get('/dashboard', function () {
-//     return view('dashboard');
-// })->middleware(['auth', 'verified'])->name('dashboard');
-
-// // Route::get('/dashboard/user', function () {
-// //     return view('user.index');
-// // })->middleware(['auth', 'verified'])->name('user.index');
-
-// Route::get('/dashboard/admin', function () {
-//     return view('admin.index');
-// })->middleware(['auth', 'verified','cekLevel'])->name('admin.index');
+Route::get('/', [BeritaController::class, 'home'])->name('home');
+Route::get('/berita', [BeritaController::class, 'index'])->name('berita.index');
+Route::get('/berita/{slug}', [BeritaController::class, 'show'])->name('berita.show');
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
