c055e89639162f4081fd6715d02dc58d9b1480b9
Gioezzy <giovanniyuda29@gmail.com>
2025-06-06 19:19:21 +0700

    feat: create AspirasiStoreRequest for validation aspirations create

---
+++ src/PengaduanOnline/app/Http/Requests/users/AspirasiStoreRequest.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Requests\users;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class AspirasiStoreRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'judul' => [
+                'required',
+                'string',
+                'max:50'
+            ],
+            'isi' => [
+                'required',
+                'string',
+                'max:1000'
+            ]
+        ];
+    }
+}
