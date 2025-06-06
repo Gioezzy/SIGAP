1876affabdefb5ceebcf752907dab605de098650
Gioezzy <giovanniyuda29@gmail.com>
2025-06-06 19:22:35 +0700

    feat: add TanggapanAspirasiController to handle responses to aspirasi

---
+++ src/PengaduanOnline/app/Http/Controllers/users/TanggapanAspirasiController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers\users;
+
+use App\Http\Controllers\Controller;
+use App\Models\TanggapanAspirasi;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class TanggapanAspirasiController extends Controller
+{
+    public function index()
+    {
+        $userId = Auth::id();
+
+        $tanggapans = TanggapanAspirasi::whereHas('aspirasi', function ($query) use ($userId) {
+            $query->where('user_id', $userId);
+        })->with('aspirasi')->latest()->get();
+
+        return view('users.tanggapanaspirasi.index', compact('tanggapans'));
+    }
+}
