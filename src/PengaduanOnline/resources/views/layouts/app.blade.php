ee847233849c6842605151f59de251ccf768b109
Lupi2602 <lupiranofan@gmail.com>
2025-06-09 17:44:29 +0700

    menghilangkan footer di tanggapan

--- src/PengaduanOnline/resources/views/layouts/app.blade.php
+++ src/PengaduanOnline/resources/views/layouts/app.blade.php
@@ -54,9 +54,10 @@
             {{ $slot }}
         </main>
 
-          @if (!request()->is('pengaduan*'))
+        @if (!request()->is('pengaduan*') && !request()->is('tanggapan*'))
             @include('layouts.footer')
         @endif
+
     </div>
 
     <!-- Bootstrap JS Bundle with Popper -->
@@ -64,8 +65,8 @@
     <script src="{{ asset('js/plugins.js') }}"></script>
     <script src="{{ asset('js/SmoothScroll.js') }}"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe"
-        crossorigin="anonymous"></script>
+        integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous">
+    </script>
     <script src="https://cdn.jsdelivr.net/npm/swiper@9/swiper-bundle.min.js"></script>
     <script src="{{ asset('js/script.min.js') }}"></script>
 </body>
