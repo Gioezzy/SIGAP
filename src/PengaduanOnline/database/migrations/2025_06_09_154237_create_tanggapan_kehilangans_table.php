42bf748a482a0044fd7449389cdc55afe9664d9b
Gioezzy <giovanniyuda29@gmail.com>
2025-06-12 21:52:11 +0700

    feat: create tanggapan_kehilangans_table for admin replies to lost items

---
+++ src/PengaduanOnline/database/migrations/2025_06_09_154237_create_tanggapan_kehilangans_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('tanggapan_kehilangans', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('id_kehilangan');
+            $table->unsignedBigInteger('user_id')->nullable();
+            $table->foreign('id_kehilangan')->references('id')->on('kehilangans')->onDelete('cascade');
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('set null');
+            $table->text('tanggapan');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('tanggapan_kehilangans');
+    }
+};
