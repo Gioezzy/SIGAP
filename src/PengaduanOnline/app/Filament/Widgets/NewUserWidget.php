fc5dc074bc6bafe58a9865154be3e67bc8306916
Gioezzy <giovanniyuda29@gmail.com>
2025-06-26 09:35:53 +0700

    feat: add new widget to view new user register in dashboard

---
+++ src/PengaduanOnline/app/Filament/Widgets/NewUserWidget.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Filament\Widgets;
+
+use App\Models\User;
+use Filament\Widgets\TableWidget as BaseWidget;
+use Filament\Tables;
+use Filament\Tables\Columns\TextColumn;
+use Illuminate\Database\Eloquent\Builder;
+
+class NewUserWidget extends BaseWidget
+{
+    protected static ?int $sort = 2;
+
+    protected function getTableQuery(): Builder
+    {
+        return User::query()->latest();
+    }
+
+    protected function getTableColumns(): array
+    {
+        return [
+            TextColumn::make('name')
+                ->label('Nama Pengguna')
+                ->searchable()
+                ->sortable(),
+
+            TextColumn::make('email')
+                ->label('Email')
+                ->copyable()
+                ->sortable(),
+
+            TextColumn::make('created_at')
+                ->label('Tanggal Daftar')
+                ->dateTime('d M Y H:i'),
+        ];
+    }
+}
